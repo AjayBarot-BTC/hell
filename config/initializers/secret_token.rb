f204e62a8b07d5c109ca4e63653c775aa7397e21
AjayBarot-BTC <ajay.barot@beaverlogic.com>
2014-06-30 14:00:01 +0530

    final

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hellday::Application.config.secret_key_base = '91e509291171b4ebfd8aa697961ef9dc785d28cf880c044040169e32afdf43f7ecc8cdffd9456de898a1e84549fec74ec5a17547a0f0592a822bab7d7468be68'
