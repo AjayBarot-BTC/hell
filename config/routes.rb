16297a1676cbbb02cbe67bc190e82ae0d6776635
AjayBarot-BTC <ajay.barot@beaverlogic.com>
2014-07-01 11:11:05 +0530

    chp 7 ended

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Hellday::Application.routes.draw do
-  get "users/new"
-  root 'static_pages#home'
+  resources :users
+  #get "users/new"
+  root 'users#new'
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
-  match '/signup', to: 'users#new', via: 'get'
+  #match '/signup', to: 'users#new', via: 'get'
+  match '/show', to: 'users#show', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
